cdce4a68de2f2e1c437522e4b1af08f8dbd55ba5
= <=>
2016-03-31 02:34:30 +0000

    first commit

---
+++ app/views/invoices/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @invoice, :id, :date, :company, :tax, :saleperson, :created_at, :updated_at
---
+++ db/migrate/20160330050617_create_invoices.rb
@@ -0,0 +1,12 @@
+class CreateInvoices < ActiveRecord::Migration
+  def change
+    create_table :invoices do |t|
+      t.datetime :date
+      t.string :company
+      t.decimal :tax
+      t.string :saleperson
+
+      t.timestamps null: false
+    end
+  end
+end
