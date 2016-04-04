74736a3bdf5f965c961e836074d7581b15a5a1bd
= <=>
2016-04-04 05:13:31 +0000

    remove sendgrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address                      =>   'smtp.sendgrid.net',
   :port                         =>   '587',
   :authentication               =>   :plain,
-  :user_name                    =>   'app49018037@heroku.com',
-  :password                     =>   'wcnfiktp5637',
+  :user_name                    =>   '',
+  :password                     =>   '',
   :domain                       =>   'heroku.com',
   :enable_stattls_auto          =>   true
   }
